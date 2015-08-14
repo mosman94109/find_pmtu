2e5ba66740f3a5c9c462e93d45b8db4bdd20154d
Michael Osman <mosman@example.com>
2015-08-14 12:34:46 +0000

    Move variables outside if; tidy up

--- find_pmtu.bash
+++ find_pmtu.bash
@@ -2,6 +2,15 @@
 
 INITIAL_SIZE=1600 # Just for testing; should start at 1500
 HOST=$1
+MAX=$INITIAL_SIZE
+MIN=1
+SIZE=$MAX
+COUNT=0
+
+function show_usage {
+    echo "Usage: $0 HOST"
+    exit 1
+}
 
 function check_ping () {
     SIZE=$1
@@ -10,29 +19,29 @@ function check_ping () {
     return $EXIT_CODE
 }
 
-MAX=$INITIAL_SIZE
-MIN=1
-SIZE=$MAX
-COUNT=0
-
-let DIFF=MAX-MIN
-while [[ $DIFF -ne 1 ]]
-do 
-    check_ping $SIZE
-    EXIT_CODE=$?
-    if [[ $EXIT_CODE -ne 0 ]]; then
-        let MAX=SIZE
+function find_mtu() {
+    let DIFF=MAX-MIN
+    while [[ $DIFF -ne 1 ]]
+    do 
+        check_ping $SIZE
+        EXIT_CODE=$?
+        if [[ $EXIT_CODE -ne 0 ]]; then
+            let MAX=SIZE
+        else
+            let MIN=SIZE
+        fi
         let SIZE=(MAX+MIN)/2
         let DIFF=MAX-MIN
         let COUNT=COUNT+1
-    else
-        let MIN=SIZE
-        let SIZE=(MAX+MIN)/2
-        let DIFF=MAX-MIN
-        let COUNT=COUNT+1
-    fi
-done
+    done
+}
 
+if [[ $# -ne 1 ]]; then
+    show_usage
+fi
+
+find_mtu
 echo "MTU to $HOST: $SIZE"
+#echo "$COUNT attempts"
 
-# To do: add usage function
+# To do: get a list of hosts from a file
