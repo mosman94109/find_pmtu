0cde35efb25cd67f01ebd1657191f5ecf48b2c6d
Michael Osman <mosman@example.com>
2015-08-14 12:40:14 +0000

    -Use UPPER and LOWER instead of MAX and MIN

--- find_pmtu.bash
+++ find_pmtu.bash
@@ -2,9 +2,9 @@
 
 INITIAL_SIZE=1600 # Just for testing; should start at 1500
 HOST=$1
-MAX=$INITIAL_SIZE
-MIN=1
-SIZE=$MAX
+UPPER_BOUND=$INITIAL_SIZE
+LOWER_BOUND=1
+SIZE=$UPPER_BOUND
 COUNT=0
 
 function show_usage {
@@ -20,18 +20,18 @@ function check_ping () {
 }
 
 function find_mtu() {
-    let DIFF=MAX-MIN
+    let DIFF=UPPER_BOUND-LOWER_BOUND
     while [[ $DIFF -ne 1 ]]
     do 
         check_ping $SIZE
         EXIT_CODE=$?
         if [[ $EXIT_CODE -ne 0 ]]; then
-            let MAX=SIZE
+            let UPPER_BOUND=SIZE
         else
-            let MIN=SIZE
+            let LOWER_BOUND=SIZE
         fi
-        let SIZE=(MAX+MIN)/2
-        let DIFF=MAX-MIN
+        let SIZE=(UPPER_BOUND+LOWER_BOUND)/2
+        let DIFF=UPPER_BOUND-LOWER_BOUND
         let COUNT=COUNT+1
     done
 }
